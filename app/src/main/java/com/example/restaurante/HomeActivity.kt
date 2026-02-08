c71e220d33f7a37073fe4d7a4f51b16599c23c62
Alan Castillo <alan@pc.com>
2026-02-08 03:28:36 -0500

    menu superior funcional

--- app/src/main/java/com/example/restaurante/HomeActivity.kt
+++ app/src/main/java/com/example/restaurante/HomeActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.example.restaurante.ui.auth.LoginActivity
 import com.example.restaurante.ui.informacion.MapsActivity
 import com.example.restaurante.ui.informacion.QuienesSomosActivity
 import com.example.restaurante.ui.informacion.VisionMisionActivity
+import com.example.restaurante.ui.mozos.GestMozosActivity
+import com.example.restaurante.ui.platos.GestPlatosActivity
+import com.example.restaurante.ui.usuarios.GestUsuariosActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
@@ -51,9 +55,33 @@ class HomeActivity : AppCompatActivity() {
     }
 
 
+    //PARA AÑADIR EL MENU SUPERIOR AL HOME
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_home, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    //PARA QUE LAS OPCIONES DEL MENU SUPERIOR FUNCIONEN PS
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.gestionUsuarios -> {
+                val pantallaGestUsu = Intent(this, GestUsuariosActivity::class.java)
+                startActivity(pantallaGestUsu)
+            }
+            R.id.gestionMozos -> {
+                val pantallaGestMoz = Intent(this, GestMozosActivity::class.java)
+                startActivity(pantallaGestMoz)
+            }
+            R.id.gestionPlatos -> {
+                val pantallaGestPlat = Intent(this, GestPlatosActivity::class.java)
+                startActivity(pantallaGestPlat)
+            }
+            R.id.gestionPedidos -> {
+                val pantallaGestPed = Intent(this, GestPedidosActivity::class.java)
+                startActivity(pantallaGestPed)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
